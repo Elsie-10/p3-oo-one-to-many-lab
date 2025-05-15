266c34168754707fee4368efb0f764fe603bc086
Elsie <elsieoscar06@gmail.com>
2025-05-15 17:32:41 +0300

    one to many

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,34 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    def __init__(self,name,pet_type,owner= None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception(f"Invalid pet_type '{pet_type}'. Must be one of {Pet.PET_TYPES}")
+
+        if owner is not None and not isinstance(owner, Owner):
+            raise Exception("owner must be an instance of Owner class")
+
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+
+        Pet.all.append(self)
+
+        
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name = name
+
+    def pets(self):
+        # return a list of pets where self is the owner
+        return[pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("pet must be an instance of the pet class")
+        pet.owner = self # assign owner to pet
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet:pet.name)
+
